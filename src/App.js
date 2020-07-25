18d86b7cbd6c81b984ee5334d7a95e3d7c4c0605
amrutha <ameeniga2019@gmail.com>
2020-07-25 21:11:14 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './App.css';
+import Calculator from '././components/calculator/index.js';
+
+const title = "Calculator";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="app-header layout-row align-items-center justify-content-center">
+          <div className="layout-row align-items-center">
+           
+            <h4 id="app-title" data-testid="app-title" className="app-title">{title}</h4>
+          </div>
+        </nav>
+        <Calculator/>
+      </div>
+    );
+  }
+}
+
+export default App;
